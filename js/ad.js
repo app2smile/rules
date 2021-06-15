10497d223266896c0e9015f13a0898f4c35e09fb
app2smile <34903735+app2smile@users.noreply.github.com>
2021-06-15 13:50:45 +0800

    Update ad.js

--- js/ad.js
+++ js/ad.js
@@ -74,7 +74,6 @@ if (url.indexOf("mi.gdt.qq.com/gdt_mview.fcg") != -1) {
     }
     body = JSON.stringify(body);
 } else if (url.indexOf("api.zhihu.com/commercial_api/real_time_launch_v2") != -1) {
-    console.log('进入zhihu,body:' + $response.body);
     body = JSON.parse($response.body);
     let launch;
     if (body.launch == undefined) {
@@ -87,21 +86,9 @@ if (url.indexOf("mi.gdt.qq.com/gdt_mview.fcg") != -1) {
         $notification.post(notifiTitle, "知乎", "launch-ads字段为undefined");
     } else {
         launch.ads = [];
-        //launch['mobile_experiment']['ad_backPlugin'] = "0";
-        //launch['mobile_experiment']['ad_bannerShow'] = "0";
-        //launch['mobile_experiment']['ad_bannerView'] = "0";
-        //launch['mobile_experiment']['ad_ht'] = "0";
-        //launch['mobile_experiment']['ad_markPlugin'] = "0";
-        //launch['mobile_experiment']['ad_new_rt'] = "0";
-        //launch['mobile_experiment']['ad_pausePlugin'] = "0";
-        //launch['mobile_experiment']['ad_pre_web'] = "0";
-        //launch['mobile_experiment']['ad_resume_ab'] = "0";
-        //launch['mobile_experiment']['new_ad_logo'] = "0";
     }
-
-    body = JSON.stringify({
-        launch: JSON.stringify(launch)
-    });
+    body.launch = JSON.stringify(launch);
+    body = JSON.stringify(body);
 } else if (url.indexOf("magev6.if.qidian.com/argus/api/v4/client/getsplashscreen") != -1) {
     //console.log('进入qidian');
     body = JSON.parse($response.body);
