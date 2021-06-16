2ebd10aeabb6415cec4984e440100d436811872f
app2smile <34903735+app2smile@users.noreply.github.com>
2021-06-16 09:29:52 +0800

    Update ad.js

--- js/ad.js
+++ js/ad.js
@@ -20,12 +20,11 @@ vgtime正则
 */
 
 let url = $request.url;
-let body;
+let body = JSON.parse($response.body);
 let notifiTitle = "去广告脚本错误";
 
 if (url.indexOf("afd.baidu.com/afd/entry") != -1 && $request.method == "GET") {
     //console.log('贴吧-进入afd');
-    body = JSON.parse($response.body);
     if (body.res == undefined || body.res.splash == undefined) {
         console.log("贴吧afd-body:" + $response.body);
         $notification.post(notifiTitle, "贴吧-afd", "res-splash字段为undefined");
@@ -33,10 +32,8 @@ if (url.indexOf("afd.baidu.com/afd/entry") != -1 && $request.method == "GET") {
         //body.res.ad = [];
         body.res.splash = null;
     }
-    body = JSON.stringify(body);
 } else if (url.indexOf("api.zhihu.com/commercial_api/real_time_launch_v2") != -1) {
     //console.log('进入知乎');
-    body = JSON.parse($response.body);
     let launch;
     if (body.launch == undefined) {
         console.log("知乎body:" + $response.body);
@@ -50,40 +47,35 @@ if (url.indexOf("afd.baidu.com/afd/entry") != -1 && $request.method == "GET") {
         launch.ads = [];
     }
     body.launch = JSON.stringify(launch);
-    body = JSON.stringify(body);
 } else if (url.indexOf("magev6.if.qidian.com/argus/api/v4/client/getsplashscreen") != -1) {
     //console.log('进入起点');
-    body = JSON.parse($response.body);
     if (body.Data == undefined || body.Data.List == undefined) {
         console.log("起点body:" + $response.body);
         $notification.post(notifiTitle, "起点", "Data/List字段为undefined");
     } else {
         body.Data.List = null;
     }
-    body = JSON.stringify(body);
 } else if (url.indexOf("api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/get_ads") != -1) {
     //console.log('进入穿山甲');
-    body = JSON.parse($response.body);
     if (body.message == undefined) {
         console.log("穿山甲body:" + $response.body);
         $notification.post(notifiTitle, "穿山甲", "message字段为undefined");
     } else {
         body.message = null;
     }
-    body = JSON.stringify(body);
 } else if (url.indexOf("app02.vgtime.com:8080/vgtime-app/api/v2/init/ad.json") != -1) {
     //console.log('进入vgtime');
-    body = JSON.parse($response.body);
     if (body.data == undefined || body.data.ad === undefined) {
         console.log("vgtime-body:" + $response.body);
         $notification.post(notifiTitle, "vgtime", "data/ad字段为undefined");
     } else {
         body.data.ad = null;
     }
-    body = JSON.stringify(body);
 } else {
     $notification.post(notifiTitle, "路径匹配错误", url);
 }
+
+body = JSON.stringify(body);
 
 $done({
     body
