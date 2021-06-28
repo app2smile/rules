d3b1f519cc72855f6345913d0c08eddb655db9d2
邹文博 <123zouwen@gmail.com>
2021-06-28 10:15:11 +0800

    fix: 起点路径问题

--- js/ad-source.js
+++ js/ad-source.js
@@ -86,7 +86,7 @@ if (url.indexOf("afd.baidu.com/afd/entry") != -1 && method == getMethod) {
         body.Data.List = null;
         console.log('成功');
     }
-} else if (url.indexOf("magev6.if.qidian.com/argus/api/v4/client/getsplashscreen") != -1 && method == getMethod) {
+} else if (url.indexOf("magev6.if.qidian.com/argus/api/v2/deeplink/geturl") != -1 && method == getMethod) {
     console.log('起点-不跳转精选');
     if (body.Data == undefined || body.Data.ActionUrl == undefined || body.Data.ActionUrl != 'QDReader://Bookstore') {
         console.log("body:" + $response.body);
