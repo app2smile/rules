4cef358cf801888c06201ceef793b93f729c9a9c
app2smile <34903735+app2smile@users.noreply.github.com>
2022-03-22 20:08:02 +0800

    Update tieba-json.js

--- js/tieba-json.js
+++ js/tieba-json.js
@@ -2,7 +2,7 @@ const url = $request.url;
 const method = $request.method;
 const getMethod = "GET";
 const postMethod = "POST";
-const notifiTitle = "去广告脚本错误";
+const notifiTitle = "贴吧json脚本错误";
 
 let body = JSON.parse($response.body);
 
