4fb10b591ed3ffdc36022f3f1471ac647668ab6c
smile <a@qq.com>
2022-04-28 10:59:18 +0800

    vgtime

---
+++ js/vgtime.js
@@ -0,0 +1,15 @@
+console.log('vgtime-开屏页');
+let body = JSON.parse($response.body);
+if (body.data === undefined || body.data.ad === undefined) {
+    console.log("body:" + $response.body);
+    $notification.post(notifiTitle, "vgtime", "data/ad字段为undefined");
+} else {
+    body.data.ad = null;
+    console.log('成功');
+}
+body = JSON.stringify(body);
+
+$done({
+    body
+});
+
