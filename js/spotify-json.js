4d30c684534d1ac1ab99a14642c62a3c28dc3c3f
app2smile <34903735+app2smile@users.noreply.github.com>
2023-06-13 14:19:12 +0800

    update

---
+++ js/spotify-json.js
@@ -0,0 +1,12 @@
+console.log(`spotify-json-2023.06.13`);
+let url = $request.url;
+// console.log(`原始url:${url}`);
+if (url.includes('platform=iphone')) {
+    url = url.replace(/platform=iphone/, 'platform=ipad');
+    // console.log(`替换platform:${url}`);
+} else {
+    console.log('无需处理');
+}
+$done({
+    url
+});
