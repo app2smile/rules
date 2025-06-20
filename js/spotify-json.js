5fa39a5fbc670e13878b774d8436f6e559850d04
app2smile <34903735+app2smile@users.noreply.github.com>
2025-06-20 17:01:08 +0800

    update spotify

--- js/spotify-json.js
+++ js/spotify-json.js
@@ -1,6 +1,9 @@
-console.log(`spotify-json-2023.06.13`);
+console.log(`spotify-json-2025.06.20`);
 let url = $request.url;
 // console.log(`原始url:${url}`);
+if (url.includes('com:443')) {
+    url = url.replace(/com:443/, 'com');
+}
 if (url.includes('platform=iphone')) {
     url = url.replace(/platform=iphone/, 'platform=ipad');
     // console.log(`替换platform:${url}`);
